ca34bf45a090c807d075bb57bbcc75bd4a488516
rahmatraywhite <rahmat.hidayat@raywhite.co.id>
2024-02-13 13:49:34 +0700

    adjust dikit

--- src/components/organism/Navbar.jsx
+++ src/components/organism/Navbar.jsx
@@ -1,101 +1,115 @@
-import { useState } from "react";
-import Brand from '../../assets/icons/brand.svg'
-import { Link } from "react-router-dom";
-import Button from "../atoms/Button";
+import { useState } from 'react';
+import Brand from '../../assets/icons/brand.svg';
+import { Link } from 'react-router-dom';
+import Button from '../atoms/Button';
 
 export default function Navbar() {
-    const [navbar, setNavbar] = useState(false);
+  const [navbar, setNavbar] = useState(false);
 
-    return (
-        <nav className="w-full bg-white shadow-lg">
-            <div className="justify-between px-4 mx-auto py-1 lg:max-w-7xl lg:items-center lg:flex 2xl:px-0">
-                <div>
-                    <div className="flex items-center justify-between py-3 lg:py-5 lg:block">
-                        <Link to='/'>
-                            <img src={Brand} alt="Valast ID" />
-                        </Link>
-                        <div className="lg:hidden">
-                            <button
-                                className="p-2 text-primary rounded-md outline-none focus:border-primary focus:border"
-                                onClick={() => setNavbar(!navbar)}
-                            >
-                                {navbar ? (
-                                    <svg
-                                        xmlns="http://www.w3.org/2000/svg"
-                                        className="w-6 h-6 text-primary"
-                                        viewBox="0 0 20 20"
-                                        fill="currentColor"
-                                    >
-                                        <path
-                                            fillRule="evenodd"
-                                            d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                                            clipRule="evenodd"
-                                        />
-                                    </svg>
-                                ) : (
-                                    <svg
-                                        xmlns="http://www.w3.org/2000/svg"
-                                        className="w-6 h-6 text-primary"
-                                        fill="none"
-                                        viewBox="0 0 24 24"
-                                        stroke="currentColor"
-                                        strokeWidth={2}
-                                    >
-                                        <path
-                                            strokeLinecap="round"
-                                            strokeLinejoin="round"
-                                            d="M4 6h16M4 12h16M4 18h16"
-                                        />
-                                    </svg>
-                                )}
-                            </button>
-                        </div>
-                    </div>
-                </div>
-                <div>
-                    <div
-                        className={`flex-1 justify-self-center pb-3 mt-8 lg:block lg:pb-0 lg:mt-0 ${navbar ? "block" : "hidden"
-                            }`}
-                    >
-                        <ul className="items-center justify-center space-y-4 lg:flex lg:space-x-[42px] lg:space-y-0">
-                            <li className="text-[#475467] font-medium hover:text-primary">
-                                <Link to={"/"}>Home</Link>
-                            </li>
-                            <li className="text-[#475467] font-medium hover:text-primary">
-                                <Link to={"/"}>Tentang Kami</Link>
-                            </li>
-                            <li className="text-[#475467] font-medium hover:text-primary">
-                                <Link to={"/"}>Solusi</Link>
-                            </li>
-                            <li className="text-[#475467] font-medium hover:text-primary">
-                                <Link to={"/"}>Klien</Link>
-                            </li>
-                            <li className="text-[#475467] font-medium hover:text-primary">
-                                <Link to={"/"}>Harga</Link>
-                            </li>
-                            <li className="text-[#475467] font-medium hover:text-primary">
-                                <Link to={"/"}>Kontak</Link>
-                            </li>
-                        </ul>
-                        <div className="mt-3 flex flex-col gap-3 lg:hidden">
-                            <Link>
-                                <Button className={"text-sm w-full bg-[#fff] border border-[#EAECF0] font-medium text-primary"}>Masuk</Button>
-                            </Link>
-                            <Link>
-                                <Button className={"text-sm w-full bg-primary font-medium text-[#fff]"}>Coba Gratis</Button>
-                            </Link>
-                        </div>
-                    </div>
-                </div>
-                <div className="hidden space-x-3 lg:inline-block">
-                    <Link>
-                        <Button className={"text-sm bg-[#fff] border border-[#EAECF0] font-medium text-primary"}>Masuk</Button>
-                    </Link>
-                    <Link>
-                        <Button className={"text-sm bg-primary font-medium text-[#fff]"}>Coba Gratis</Button>
-                    </Link>
-                </div>
+  return (
+    <nav className="w-full bg-white shadow-lg">
+      <div className="justify-between px-4 mx-auto py-1 lg:max-w-7xl lg:items-center lg:flex 2xl:px-0">
+        <div>
+          <div className="flex items-center justify-between py-3 lg:py-5 lg:block">
+            <Link to="/">
+              <img src={Brand} className="w-[118px] h-[28px]" alt="Valast ID" />
+            </Link>
+            <div className="lg:hidden">
+              <button
+                className="p-2 text-primary rounded-md outline-none focus:border-primary focus:border"
+                onClick={() => setNavbar(!navbar)}>
+                {navbar ? (
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="w-6 h-6 text-primary"
+                    viewBox="0 0 20 20"
+                    fill="currentColor">
+                    <path
+                      fillRule="evenodd"
+                      d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                      clipRule="evenodd"
+                    />
+                  </svg>
+                ) : (
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="w-6 h-6 text-primary"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    strokeWidth={2}>
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M4 6h16M4 12h16M4 18h16"
+                    />
+                  </svg>
+                )}
+              </button>
             </div>
-        </nav>
-    );
+          </div>
+        </div>
+        <div>
+          <div
+            className={`flex-1 justify-self-center pb-3 mt-8 lg:block lg:pb-0 lg:mt-0 ${
+              navbar ? 'block' : 'hidden'
+            }`}>
+            <ul className="items-center justify-center space-y-4 lg:flex lg:space-x-[42px] lg:space-y-0">
+              <li className="text-[#475467] font-medium hover:text-primary">
+                <Link to={'/'}>Home</Link>
+              </li>
+              <li className="text-[#475467] font-medium hover:text-primary">
+                <Link to={'/'}>Tentang Kami</Link>
+              </li>
+              <li className="text-[#475467] font-medium hover:text-primary">
+                <Link to={'/'}>Solusi</Link>
+              </li>
+              <li className="text-[#475467] font-medium hover:text-primary">
+                <Link to={'/'}>Klien</Link>
+              </li>
+              <li className="text-[#475467] font-medium hover:text-primary">
+                <Link to={'/'}>Harga</Link>
+              </li>
+              <li className="text-[#475467] font-medium hover:text-primary">
+                <Link to={'/'}>Kontak</Link>
+              </li>
+            </ul>
+            <div className="mt-3 flex flex-col gap-3 lg:hidden">
+              <Link>
+                <Button
+                  className={
+                    'text-sm w-full bg-[#fff] border border-[#EAECF0] font-medium text-primary'
+                  }>
+                  Masuk
+                </Button>
+              </Link>
+              <Link>
+                <Button
+                  className={
+                    'text-sm w-full bg-primary font-medium text-[#fff]'
+                  }>
+                  Coba Gratis
+                </Button>
+              </Link>
+            </div>
+          </div>
+        </div>
+        <div className="hidden space-x-3 lg:inline-block">
+          <Link>
+            <Button
+              className={
+                'text-sm bg-[#fff] border border-[#EAECF0] font-medium text-primary'
+              }>
+              Masuk
+            </Button>
+          </Link>
+          <Link>
+            <Button className={'text-sm bg-primary font-medium text-[#fff]'}>
+              Coba Gratis
+            </Button>
+          </Link>
+        </div>
+      </div>
+    </nav>
+  );
 }
--- src/components/organism/TryValast.jsx
+++ src/components/organism/TryValast.jsx
@@ -6,39 +6,69 @@ import HeaderRight from '../../assets/images/header right.png';
 import HeaderLeft from '../../assets/images/melengkung.svg';
 
 const TryValast = () => {
-    return (
-        <Fragment>
-            <div style={{ background: 'linear-gradient(180deg, rgba(60, 128, 242, 0.08) 27.24%, rgba(60, 128, 242, 0.00) 100%)' }}>
-                <div className='flex flex-col md:flex-row justify-center items-center py-5 px-4 lg:px-8'>
-                    <div className='text-center lg:text-left'>
-                        <p className='text-[#1D2939] font-extrabold text-[28px] lg:text-[38px] leading-[36px] lg:leading-[52px]'>
-                            Kamu Mempunyai Kesulitan Saat Menggunakan Valast?
-                        </p>
-                        <p className='text-[#475467] mt-4 text-lg lg:text-xl leading-6'>Dapatkan Informasi lebih lengap tentang Valast</p>
-                        <div className='flex flex-col lg:flex-row items-center mt-8 gap-3'>
-                            <Button className='bg-[#3C80F2] text-white'>Jalankan Demo</Button>
-                            <Button className='bg-[#fff] border border-[#EAECF0] text-primary'>Hubungi Kami</Button>
-                        </div>
-                    </div>
-                    <img src={Custry} alt='Coba Sekarang' className='mt-6 lg:mt-0 lg:ml-8' />
-                </div>
+  return (
+    <Fragment>
+      <div
+        style={{
+          background:
+            'linear-gradient(180deg, rgba(60, 128, 242, 0.08) 27.24%, rgba(60, 128, 242, 0.00) 100%)',
+        }}>
+        <div className="flex flex-col md:flex-row justify-center items-center py-5 px-4 lg:px-8">
+          <div className="text-center lg:text-left">
+            <p className="text-[#1D2939] font-extrabold text-[28px] lg:text-[38px] leading-[36px] lg:leading-[52px]">
+              Kamu Mempunyai Kesulitan Saat Menggunakan Valast?
+            </p>
+            <p className="text-[#475467] mt-4 text-lg lg:text-xl leading-6">
+              Dapatkan Informasi lebih lengap tentang Valast
+            </p>
+            <div className="flex flex-col lg:flex-row items-center mt-8 gap-3">
+              <Button className="bg-[#3C80F2] text-white">Jalankan Demo</Button>
+              <Button className="bg-[#fff] border border-[#EAECF0] text-primary">
+                Hubungi Kami
+              </Button>
             </div>
-            <div className='bg-[#3C80F2] relative py-6 lg:py-12'>
-                <div className='max-w-7xl mx-auto px-4 lg:px-8 flex flex-col lg:flex-row items-center justify-center lg:justify-between'>
-                    <img src={HeaderLeft} className='absolute left-0 top-0 hidden lg:block' alt="" />
-                    <div className='text-center lg:text-left'>
-                        <img src={Valas2} alt="Valast ID" className='mx-auto lg:mx-0' />
-                        <p className='text-white text-[22px] lg:text-[42px] font-bold mt-4 lg:mt-[81px] leading-[26px] lg:leading-[52px]'>Kelola Semua Urusan Bisnis Menggunakan Valast </p>
-                        <div className='flex flex-col lg:flex-row items-center justify-center lg:justify-start mt-6 lg:mt-8 gap-3'>
-                            <Button className='bg-[#fff] text-primary'>Coba Gratis 30 Hari</Button>
-                            <Button className='bg-transparent border border-[#fff] text-white'>Coba Sekarang</Button>
-                        </div>
-                    </div>
-                    <img className='max-w-full lg:max-w-[525px] mt-8 lg:mt-0' src={HeaderRight} alt="Try Valast" />
-                </div>
+          </div>
+          <img
+            src={Custry}
+            alt="Coba Sekarang"
+            className="mt-6 lg:mt-0 lg:ml-8"
+          />
+        </div>
+      </div>
+      <div className=" relative ">
+        <div className="max-w-7xl p-8 bg-[#3C80F2] mx-auto rounded-2xl flex flex-col lg:flex-row items-center justify-center lg:justify-between">
+          <img
+            src={HeaderLeft}
+            className="absolute -left-0 2xl:left-60 top-0 hidden lg:block"
+            alt=""
+          />
+          <div className="text-center lg:text-left">
+            <img
+              src={Valas2}
+              alt="Valast ID"
+              className="mx-auto w-[118px] h-[23.7px] lg:mx-0"
+            />
+            <p className="text-white text-[22px] lg:text-[42px] font-bold mt-4 lg:mt-[81px] leading-[26px] lg:leading-[52px]">
+              Kelola Semua Urusan Bisnis Menggunakan Valast{' '}
+            </p>
+            <div className="flex flex-col lg:flex-row items-center justify-center lg:justify-start mt-6 lg:mt-8 gap-3">
+              <Button className="bg-[#fff] text-primary">
+                Coba Gratis 30 Hari
+              </Button>
+              <Button className="bg-transparent border border-[#fff] text-white">
+                Coba Sekarang
+              </Button>
             </div>
-        </Fragment>
-    );
+          </div>
+          <img
+            className="max-w-full lg:max-w-[525px] mt-8 lg:mt-0"
+            src={HeaderRight}
+            alt="Try Valast"
+          />
+        </div>
+      </div>
+    </Fragment>
+  );
 };
 
 export default TryValast;
