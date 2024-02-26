92cf20054c46f368707f84c6fcaf75d0a5494a1f
rahmatraywhite <rahmat.hidayat@raywhite.co.id>
2024-02-26 10:26:31 +0700

    adjust cardhome

--- src/components/atoms/CardHome.jsx
+++ src/components/atoms/CardHome.jsx
@@ -10,7 +10,7 @@ export const CardHome = (props) => {
       data-aos="fade-up"
       className="bg-[#3C80F2] bg-opacity-[2%] p-8 flex flex-col items-center border border-[#EAECF0] rounded-xl justify-center">
       <img className="w-[80px] h-[80px]" src={img} alt={img} />
-      <p className="text-2xl font-semibold leading-6 text-center w-[311px] mt-8 mb-4 text-[#1D2939]">
+      <p className="text-[22px] font-semibold leading-6 text-center w-[311px] mt-8 mb-4 text-[#1D2939]">
         {title}
       </p>
       <p className="text-[#667085] text-base leading-6 w-[311px] text-center">
